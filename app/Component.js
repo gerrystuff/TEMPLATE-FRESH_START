59ab74ca4d2cd9146d3e6970549d37f518cb05e1
gerry <luisgerardoaguilarpadilla@gmail.com>
2021-01-16 00:20:01 -0700

    responsive

--- app/Component.js
+++ app/Component.js
@@ -1,16 +1,21 @@
 export default class Component {
-    constructor({selector, dimensions}) {
+    constructor({selector, size}) {
+        this.size = size;
         this.element = document.querySelector(selector);
-        this.element.classList.add('board');
+        this.element.classList.add('component');
+        this.init();
+    }
 
-        if(dimensions){
-            this.element.style.width= dimensions.width;
-            this.element.style.height = dimensions.height;
+    init(){
+        if(this.size){
+            this.element.style.width= this.size;
+            this.element.style.height = this.size;
         } else {
+            const size = 'calc(90vmin)';
             this.element.style.width= `90vmin`;
             this.element.style.height = `90vmin`;
         }
-        
+            
     }
 }
 
